4aed3c13ca626a9538f349ca7cca2383a6c542cd
Jake Torres <jakert2244@gmail.com>
2021-09-18 21:47:53 -0400

    juifjsbkfsbkfsj

--- app.js
+++ app.js
@@ -15,6 +15,46 @@ var directionsRenderer = new google.maps.DirectionsRenderer();
 
 directionsRenderer.setMap(map);
 
+
 function calcRoute() {
-    
+    var request = {
+        origin: document.getElementById("from").value,
+        destination: document.getElementById("to").value,
+        travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+    }
+
+    //pass the request to the route method
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+
+            //Get distance and time
+            const output = document.querySelector('#output');
+            output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
+
+            //display route
+            directionsRenderer.setDirections(result);
+        } else {
+            //delete route from map
+            directionsRenderer.setDirections({ routes: [] });
+            //center map in London
+            map.setCenter(myLatLng);
+
+            //show error message
+            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
+        }
+    });
 }
+
+
+var options = {
+    types: ['(cities)'] 
+}
+
+var input1 = document.getElementById("from");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+
+var input2 = document.getElementById("to");
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+
+
